27a1129ff367c000b7e921e771e12aea1a4d6ee8
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-03-31 20:11:04 +0300

    feat: add npm run commands

--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -1,39 +1,43 @@
-import 'phaser';
+import "phaser";
 
 export default class MainScene extends Phaser.Scene {
-  constructor() {
-    super('main')
-  }
+    constructor() {
+        super("main");
+    }
 
-  preload() {
-    this.load.setBaseURL('https://labs.phaser.io')
+    preload() {
+        this.load.setBaseURL("https://labs.phaser.io");
 
-    this.load.image('sky', 'assets/skies/space3.png')
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-    this.load.image('red', 'assets/particles/red.png')
-  }
+        this.load.image("sky", "assets/skies/space3.png");
+        this.load.image("logo", "assets/sprites/phaser3-logo.png");
+        this.load.image("red", "assets/particles/red.png");
+    }
 
-  create() {
-    let image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'sky')
-    let scaleX = this.cameras.main.width / image.width
-    let scaleY = this.cameras.main.height / image.height
-    let scale = Math.max(scaleX, scaleY)
-    image.setScale(scale).setScrollFactor(0)
+    create() {
+        let image = this.add.image(
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 2,
+            "sky"
+        );
+        let scaleX = this.cameras.main.width / image.width;
+        let scaleY = this.cameras.main.height / image.height;
+        let scale = Math.max(scaleX, scaleY);
+        image.setScale(scale).setScrollFactor(0);
 
-    const particles = this.add.particles('red')
+        const particles = this.add.particles("red");
 
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD',
-    })
+        const emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: "ADD",
+        });
 
-    const logo = this.physics.add.image(400, 100, 'logo')
+        const logo = this.physics.add.image(400, 100, "logo");
 
-    logo.setVelocity(100, 200)
-    logo.setBounce(1, 1)
-    logo.setCollideWorldBounds(true)
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo)
-  }
+        emitter.startFollow(logo);
+    }
 }
